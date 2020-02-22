ced868d411fd50271d8daa809a68f09e1c746431
coy5z9 <pobertbraun@yandex.ru>
2020-02-22 12:46:46 +0300

    V principe namana

--- blog/urls.py
+++ blog/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.ArticleView.as_view(), name='article'),
     path('<int:pk>/new_comment', views.NewComment.as_view(), name='new_comment'),
-    #
-    # path('login/', LoginView.as_view(), name='login'),
-    # path('logout/', LogoutView.as_view(), name='logout'),
-    #
-    # path('signup/', views.SignUp.as_view(), name='signup'),
+
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+    path('signup/', views.SignUp.as_view(), name='signup'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -24,29 +24,22 @@ class ArticleView(generic.DetailView):
     template_name = 'article.html'
 
 
-class NewComment(CreateView):
+class NewComment(LoginRequiredMixin, CreateView):
     model = Comment
     form_class = CommentForm
     template_name = 'new_comment_form.html'
+    login_url = reverse_lazy('blog:login')
+    redirect_field_name = REDIRECT_FIELD_NAME
 
     def form_valid(self, form):
         form.instance.article = get_object_or_404(Article, *self.args, **self.kwargs)
         return super(NewComment, self).form_valid(form)
 
-# class NewComment(LoginRequiredMixin, CreateView):
-#     model = Comment
-#     form_class = CommentForm
-#     template_name = 'new_comment_form.html'
-#     login_url = reverse('blog:login')
-#     redirect_field_name = REDIRECT_FIELD_NAME
-#
-#     def form_valid(self, form):
-#         form.instance.article = get_object_or_404(Article, *self.args, **self.kwargs)
-#         return super(NewComment, self).form_valid(form)
-#
-#
-# class SignUp(CreateView):
-#     form_class = UserCreationForm
-#     template_name = 'registration/signup.html'
-#     success_url = reverse('blog:index')
 
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('blog:index')
+
+    def form_valid(self, form):
+        return super(SignUp, self).form_valid(form)
